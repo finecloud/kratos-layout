2506dedf9cd3c3a040ada84d371a4a54ca9173e3
Rainy <houshuai0816@126.com>
2023-03-29 15:01:41 +0800

    fix: 完善模版工程初始化逻辑

--- go.mod
+++ go.mod
@@ -3,30 +3,22 @@ module github.com/author_name/project_name
 go 1.16
 
 require (
-	github.com/AlecAivazis/survey/v2 v2.3.2 // indirect
 	github.com/alibaba/sentinel-golang v1.0.3
 	github.com/alibaba/sentinel-golang/pkg/datasource/nacos v0.0.0-20210922020954-ace810bc3806
 	github.com/casdoor/casdoor-go-sdk v0.2.0
-	github.com/emicklei/proto v1.9.2 // indirect
-	github.com/go-kratos/kratos/cmd/kratos/v2 v2.0.0-20220309025117-4387085047b9 // indirect
-	github.com/go-kratos/kratos/cmd/protoc-gen-go-errors/v2 v2.0.0-20220309025117-4387085047b9 // indirect
-	github.com/go-kratos/kratos/cmd/protoc-gen-go-http/v2 v2.0.0-20220309025117-4387085047b9 // indirect
 	github.com/go-kratos/kratos/contrib/metrics/prometheus/v2 v2.0.0-20220227042951-48d407cc9bd9
 	github.com/go-kratos/kratos/v2 v2.2.0
 	github.com/go-kratos/nacos v0.1.1
 	github.com/go-kratos/swagger-api v1.0.1
 	github.com/google/wire v0.5.0
-	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/gorilla/handlers v1.5.1
 	github.com/nacos-group/nacos-sdk-go v1.0.9
 	github.com/prometheus/client_golang v1.9.0
 	github.com/spf13/afero v1.8.1 // indirect
 	github.com/spf13/viper v1.10.0
-	golang.org/x/mod v0.5.1 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sys v0.0.0-20220307203707-22a9840ba4d7 // indirect
-	google.golang.org/genproto v0.0.0-20220308174144-ae0e22291548
-	google.golang.org/grpc v1.44.0
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
+	google.golang.org/genproto v0.0.0-20220308174144-ae0e22291548 // indirect
 	google.golang.org/protobuf v1.27.1
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 	gorm.io/driver/mysql v1.1.2
--- internal/server/http.go
+++ internal/server/http.go
@@ -1,6 +1,11 @@
 package server
 
 import (
+	"github.com/gorilla/handlers"
+	"google.golang.org/protobuf/proto"
+	http2 "net/http"
+
+	"encoding/json"
 	"github.com/author_name/project_name/internal/conf"
 	"github.com/author_name/project_name/internal/data"
 	"github.com/author_name/project_name/pkg/casdoor_auth"
@@ -15,6 +20,7 @@ import (
 	"github.com/go-kratos/kratos/v2/transport/http"
 	"github.com/go-kratos/swagger-api/openapiv2"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 // NewHTTPServer new HTTP server.
@@ -32,6 +38,38 @@ func NewHTTPServer(c *conf.Server, auth *conf.Data, logger log.Logger) *http.Ser
 				metrics.WithSeconds(prom.NewHistogram(data.MetricReqDurationHistogram)),
 				metrics.WithRequests(prom.NewCounter(data.MetricReqTotalCounter))),
 		),
+		http.ResponseEncoder(func(w http2.ResponseWriter, request *http2.Request, v interface{}) error {
+			pbJson := func(v interface{}) ([]byte, error) {
+				mOption := protojson.MarshalOptions{
+					UseProtoNames:   true,
+					EmitUnpopulated: true,
+					UseEnumNumbers:  true,
+				}
+				if m, ok := v.(proto.Message); ok {
+					return mOption.Marshal(m)
+				} else {
+					return json.Marshal(v)
+				}
+			}
+			data, err := pbJson(struct {
+				Code    int         `json:"code"`
+				Message string      `json:"message"`
+				Data    interface{} `json:"data"`
+			}{
+				Code:    0,
+				Message: "success",
+				Data:    v,
+			})
+			if err != nil {
+				return err
+			}
+			w.Header().Set("Content-Type", "application/json")
+			_, err = w.Write(data)
+			if err != nil {
+				return err
+			}
+			return nil
+		}),
 	}
 	if c.Http.Network != "" {
 		opts = append(opts, http.Network(c.Http.Network))
@@ -42,6 +80,11 @@ func NewHTTPServer(c *conf.Server, auth *conf.Data, logger log.Logger) *http.Ser
 	if c.Http.Timeout != nil {
 		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
 	}
+	opts = append(opts, http.Filter(handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "HEAD", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Card", "GroupId", "X-Requested-With", "Access-Control-Allow-Credentials", "User-Agent", "Content-Length", "Authorization"}),
+	)))
 	srv := http.NewServer(opts...)
 
 	openAPIhandler := openapiv2.NewHandler()
